2642a21cc8fa895c8f4a5487fbdcce202fa4d71d
seangaff <gaffnese@gmail.com>
2023-02-07 16:38:48 +0000

    Added and testing Firebase

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,20 +1,63 @@
-import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { initializeApp } from "firebase/app";
 
-export default function App() {
-    return (
-        <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto"/>
-        </View>
-    );
-}
+import { getFirestore } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect,useState } from "react";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyDuCfCrzKwNr76dXBYy0A1ZQNigj-rz2aQ",
+	authDomain: "barter-box-a2d9f.firebaseapp.com",
+	projectId: "barter-box-a2d9f",
+	storageBucket: "barter-box-a2d9f.appspot.com",
+	messagingSenderId: "485483637136",
+	appId: "1:485483637136:web:be1104ddc3cab4e4676b15",
+	measurementId: "G-758NTMNZXJ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+const App = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const querySnapshot = getDocs(collection(db, "testData"))
+      .then(snapshot => {
+        const data = [];
+        snapshot.forEach(doc => {
+          console.log(doc.id, " => ", doc.data().Item);
+          data.push(doc.data().Item);
+        });
+        setItems(data);
+      })
+      .catch(error => {
+        console.log("Error getting documents: ", error);
+      });
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>All Items:</Text>
+      {items.map((item, index) => (
+        <Text key={index}>{item}</Text>
+      ))}
+      <StatusBar style="auto" />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
+
+export default App;
