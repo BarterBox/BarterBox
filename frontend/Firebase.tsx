3dcb54944046b4b003c2a5cddd11bddaa8437a6c
Chafficui <felixbeinssen@gmail.com>
2023-02-27 17:47:13 +0000

    added qol functions to firebase

--- frontend/Firebase.tsx
+++ frontend/Firebase.tsx
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app";
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/storage'
+import {where, query, collection, doc, getDoc, getDocs, getFirestore} from "firebase/firestore";
+import WhereFilterOp = firebase.firestore.WhereFilterOp;
 
 const firebaseConfig = {
 	apiKey: "AIzaSyDuCfCrzKwNr76dXBYy0A1ZQNigj-rz2aQ",
@@ -19,5 +21,21 @@ if (!firebase.apps.length) {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const storage = firebase.storage()
+const db = getFirestore(app);
 
-export {app, storage}
+export {app, storage, db}
+
+export async function getFirestoreDocumentData(path: string) {
+	return await getDoc(doc(db, path)).then((doc) => {
+		if (doc.exists()) {
+			return doc.data()
+		} else {
+			return null
+		}
+	})
+}
+
+export async function getFirestoreCollectionDataWhere(path: string, field: string, operator: WhereFilterOp, value: string) {
+	const querySnapshot = await getDocs(query(collection(db, path), where(field, operator, value)));
+	return querySnapshot.docs.map(doc => doc.data())
+}
