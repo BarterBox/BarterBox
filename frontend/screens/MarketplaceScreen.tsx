9bef0730ba5fdcd12af3e2c6b504b43b46ffb8e5
sarahdolan1 <sarahdolan1969@hotmail.com>
2023-03-01 15:55:49 +0000

    not showing up when searched but works in console

--- frontend/screens/MarketplaceScreen.tsx
+++ frontend/screens/MarketplaceScreen.tsx
@@ -1,19 +1,47 @@
 import React, {useContext, useEffect, useState} from 'react';
-import {View, StyleSheet, ScrollView} from 'react-native';
+import {View, StyleSheet, ScrollView, Text} from 'react-native';
 import {AuthContext} from '../navigation/AuthProvider';
 import Heading1 from "../components/Heading1";
 import Item from "../types/Item";
 import {getFirestoreCollectionDataWhere} from "../Firebase";
 import MarketplaceItemCard from "../components/marketplace-screen/MarketplaceItemCard";
+import { SearchBar } from '@rneui/themed';
 
 const MarketplaceScreen = () => {
     const {user} = useContext(AuthContext);
     const [items, setItems] = useState<Item[]>([]);
+    const [search, setSearch] = useState("");
 
     const fetchItems = async () => {
         const items = await getFirestoreCollectionDataWhere("items", "owner", "!=", user.uid);
         setItems(items as Item[]);
     }
+    const clearSearchedItems = () => {
+        searchedItems = new Array();
+        for(var i=0; i<items.length; i++){
+           searchedItems.push(items[i]);
+       }
+     };
+
+     const updateSearch = (search) => {
+        setSearch(search);
+        clearSearchedItems();
+        searchData(search);
+       
+      };
+      let searchedItems = [];
+      const searchData = (search) => {
+          searchedItems = new Array();
+          for(var i=0; i<items.length; i++){
+              if(items[i].heading.startsWith(search)){
+                  searchedItems.push(items[i]);
+              }
+          }
+          console.log(searchedItems.length);
+  
+  
+      }
+  
 
     useEffect(() => {
         fetchItems()
@@ -22,6 +50,13 @@ const MarketplaceScreen = () => {
     return (
         <View style={styles.container}>
             <Heading1 text="Marketplace"/>
+            <SearchBar
+          placeholder="Seach Marketplace ..."
+          onChangeText={updateSearch}
+          value={search}
+          
+        />
+
             <ScrollView onScrollToTop={fetchItems} style={styles.itemsContainer} contentContainerStyle={styles.scrollBarItemsContainer}>
                 {items.map((item, index) => {
                     return <MarketplaceItemCard key={index} item={item}/>
