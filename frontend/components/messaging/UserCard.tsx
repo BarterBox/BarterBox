57f1e1b48270164cf9a5f0cd5ba6ab495bfcea4e
aodhanocathain <aodhanocathain@gmail.com>
2023-03-06 12:18:25 +0000

    Show list of other users, with whom the current user has opened chats, on chats screen

--- frontend/components/messaging/UserCard.tsx
+++ frontend/components/messaging/UserCard.tsx
@@ -1,7 +1,10 @@
-import React, { StyleSheet, Image, Text, TouchableOpacity } from "react-native";
+import React, { View, StyleSheet, Image, Text, TouchableOpacity } from "react-native";
 import User from "../../types/User";
 
 export default function UserCard({ user, onPress }: { user: User, onPress: () => void }) {
+    if (!(user.photoURL)) {
+        user.photoURL = "https://firebasestorage.googleapis.com/v0/b/barter-box-a2d9f.appspot.com/o/images%2Fquestionmark.png?alt=media&token=ea7e9ace-47e2-461f-9c33-3e4d23f57ec9";
+    }
     return (
         <TouchableOpacity onPress={onPress} style={styles.container} >
             <Image style={styles.image} source={{ uri: user.photoURL }} />
@@ -10,29 +13,30 @@ export default function UserCard({ user, onPress }: { user: User, onPress: () =>
     )
 }
 
+const cardBorderWidth = 5;
+const imgDims = 40;
+const imgOffset = 5;
+const nameOffset = 15;
+
 const styles = StyleSheet.create({
     container: {
         flexDirection: "row",
         borderRadius: 10,
-        height: 100,
-        width: "100%",
-        borderWidth: 5,
-        borderColor: "#2AF"
+        height: (cardBorderWidth + imgOffset + imgDims + imgOffset + cardBorderWidth),
+        width: 300,
+        borderWidth: cardBorderWidth,
+        borderColor: "#2AF",
+        alignItems: "center",
+        backgroundColor: "#BEF"
     },
     image: {
-        width: 80,
-        height: 80,
-        position: "relative",
-        top: 10,
-        left: 10,
+        left: imgOffset,
+        borderRadius: 10,
+        width: imgDims,
+        height: imgDims,
     },
     name: {
-        top: 30,
-        left: 30,
-        fontSize: 20,
-        fontWeight: "bold",
-        position: "relative",
-        textAlign: "center",
-        justifyContent: "center"
+        left: nameOffset,
+        fontSize: imgDims / 1.5,    //the division is arbitrary
     }
 });
--- frontend/screens/ChatsScreen.tsx
+++ frontend/screens/ChatsScreen.tsx
@@ -1,10 +1,11 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { View, Button } from 'react-native';
+import { View, Button, FlatList } from 'react-native';
 import { StyleSheet } from 'react-native';
 import Heading1 from "../components/Heading1";
 import { AuthContext } from '../navigation/AuthProvider';
 import { app } from '../Firebase';
-import { getFirestore, getDoc, doc } from "firebase/firestore";
+import { getFirestore, getDoc, getDocs, doc, query, collection, where } from "firebase/firestore";
+import { getFirestoreCollectionDataWhere } from "../Firebase";
 
 import UserCard from "../components/messaging/UserCard";
 
@@ -15,29 +16,56 @@ const ChatsScreen = ({ navigation }) => {
     const { user } = useContext(AuthContext);
 
     const [userData, setUserData] = useState({
-        displayName: null, photoURL: null, email: null
+        displayName: "User", photoURL: null, email: null
     });
 
+    const [correspondants, setCorrespondants] = useState([]);
+
     useEffect(() => {
-        getDoc(doc(database, "Users", user.uid))
-            .then(snapshot => { return snapshot.data(); })
-            .then(data => { setUserData({ displayName: data.displayName, photoURL: data.photoURL, email: data.email }); })
-            .catch(error => { setUserData({ displayName: "n/a", photoURL: null, email: null }) });
+        (async () => {
+            //get data for the current user
+            const userSnapshot = await getDoc(doc(database, "Users", user.uid));
+            const data = userSnapshot.data();
+            const { displayName, photoURL = null, email } = data;
+            setUserData({ displayName: displayName, photoURL: photoURL, email: email });
+
+            //get the users with whom the current user has opened a chat, as user1 or as user2 in the database
+            const correspondants = [];
+            let index = 0;
+            async function addCorrespondants(userRole) {
+                const correspondantRole = (userRole == "user1") ? "user2" : "user1";
+                const chats = await getFirestoreCollectionDataWhere("chats", userRole, "==", user.uid);
+                for (const chat of chats) {
+                    const correspondantSnapshot = await getDoc(doc(database, "Users", chat[correspondantRole]));
+                    const data = correspondantSnapshot.data();
+                    const { displayName, photoURL = null, email } = data;
+                    correspondants.push({ "id": index, "correspondant": { displayName: displayName, photoURL: photoURL, email: email } });
+                    index++;
+                }
+            }
+            await addCorrespondants("user1");
+            await addCorrespondants("user2");
+            setCorrespondants(correspondants);
+        })();
     }, []);
 
-    const { displayName, photoURL, email } = userData;
-    const userObj = { displayName: displayName, photoURL: photoURL, email: email }
     return (
         <View style={styles.container}>
             <Heading1 text={`${userData.displayName}'s chats`}></Heading1>
-            <UserCard user={userObj} onPress={() => { navigation.navigate("Messaging") }} />
+            <FlatList
+                data={correspondants}
+                renderItem={({ item }) => { return <UserCard user={item.correspondant} onPress={() => { navigation.navigate("Messaging") }} /> }}
+                ItemSeparatorComponent={({ }) => {
+                    return <View style={{ height: 5 }}></View>;
+                }}
+            />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        top: 35,
         backgroundColor: "#fff",
         alignItems: "center",
         justifyContent: "center",
