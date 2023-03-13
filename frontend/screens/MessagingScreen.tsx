8cff36f15807b92b49f5b3df16aed48999e4fc91
aodhanocathain <aodhanocathain@gmail.com>
2023-03-13 12:18:31 +0000

    Add message writing and sending

--- frontend/screens/MessagingScreen.tsx
+++ frontend/screens/MessagingScreen.tsx
@@ -1,8 +1,8 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { View, Button, FlatList, Text } from 'react-native';
+import { View, KeyboardAvoidingView, Button, FlatList, Text, TextInput } from 'react-native';
 import { StyleSheet } from 'react-native';
 import { app } from '../Firebase';
-import { getFirestore, getDoc, getDocs, doc, query, collection, where } from "firebase/firestore";
+import { getFirestore, getDoc, getDocs, setDoc, doc, query, collection, where, onSnapshot } from "firebase/firestore";
 
 const database = getFirestore(app);
 
@@ -10,62 +10,74 @@ const MessagingScreen = ({ navigation, route }) => {
 
     const [messages, setMessages] = useState([]);
 
+    const [input, setInput] = useState("");
+
     useEffect(() => {
         (async () => {
             const messagesQuery = query(collection(database, `chats/${route.params.chat.id}/messages`));
-            const messagesDocs = await getDocs(messagesQuery);
-
-            setMessages(messagesDocs.docs
-                .filter((document) => { return document.data().content && true })
-                .map((document, index) => { return { id: index, message: document.data() } })
-            )
+            //check for updates regularly
+            setInterval(async () => {
+                const messagesDocs = await getDocs(messagesQuery);
+                setMessages(messagesDocs.docs
+                    .filter((document) => { return document.data().content && true })
+                    .map((document, index) => { return { id: index, message: document.data() } })
+                )
+            }, 1000);
         })();
     }, []);
 
     return (
-        <View style={styles.container}>
-            <View style={styles.banner}>
+        <KeyboardAvoidingView behavior="position" style={styles.container}>
+            <View style={{ borderColor: "#000", borderWidth: 5 }}>
                 <Button title="Go back" onPress={() => { navigation.navigate("Chats") }}></Button>
             </View>
-            <View>
-                <FlatList
-                    data={messages}
-                    renderItem={({ item }) => {
-                        if (item.message.sender == route.params.userid) {
-                            return <Text style={styles.user} >{`You: ${item.message.content}`}</Text>
-                        }
-                        else {
-                            return <Text style={styles.correspondant} >{`${route.params.chat.correspondant.displayName}: ${item.message.content}`}</Text>
-                        }
-                    }}
-                />
+            <FlatList
+                data={messages}
+                renderItem={({ item }) => {
+                    if (item.message.sender == route.params.userid) {
+                        return <Text style={styles.user} >{`You: ${item.message.content}`}</Text>
+                    }
+                    else {
+                        return <Text style={styles.correspondant} >{`${route.params.chat.correspondant.displayName}: ${item.message.content}`}</Text>
+                    }
+                }}
+            />
+            <View style={{ borderColor: "#000", borderWidth: 1 }}>
+                <TextInput placeholder="Enter a message" value={input} onChangeText={(text) => { setInput(text); }}></TextInput>
             </View>
-        </View>
+            <View style={{ borderColor: "#000", borderWidth: 5 }}>
+                <Button title="Send message" onPress={() => {
+                    //unix millis to have really low chance of two documents writing with the same id (lazy solution)
+                    setDoc(doc(database, `chats/${route.params.chat.id}/messages`, `${Date.now()}`), {
+                        content: input,
+                        sender: route.params.userid
+                    })
+                    setInput("");
+                }}></Button>
+            </View>
+        </KeyboardAvoidingView>
     );
 
 }
 
 const fontsize = 20;
+const topMargin = 35;
+const bottomMargin = 15;
 
 const styles = StyleSheet.create({
     container: {
-        top: 35,
+        top: topMargin,
+        bottom: bottomMargin,
         backgroundColor: "#fff",
-    },
-    banner: {
-        flexDirection: "column",
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
     },
     user: {
         fontSize: fontsize,
-        backgroundColor: "#BEF",
+        backgroundColor: "#DFF",
     },
     correspondant: {
         fontSize: fontsize,
-        backgroundColor: "#6BF",
-    }
+        backgroundColor: "#BEF",
+    },
 });
 
 export default MessagingScreen;
