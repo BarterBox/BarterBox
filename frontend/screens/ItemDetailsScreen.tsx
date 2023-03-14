864c6c5db769985ee685ad4a3644766b7a7a1032
Chafficui <felixbeinssen@gmail.com>
2023-03-14 18:05:48 +0000

    styled item details

--- frontend/screens/ItemDetailsScreen.tsx
+++ frontend/screens/ItemDetailsScreen.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
-import { View, Text, Button, Alert, Image } from 'react-native';
-import { StyleSheet } from 'react-native';
-import Item from "../types/Item";
+import {View, Text, Alert, Image} from 'react-native';
+import {Button, Colors} from 'react-native-ui-lib';
+import {StyleSheet} from 'react-native';
+import Background from "../components/general/Background";
 
 
-const ItemDetailsScreen = ({ navigation, route }) => {
+const ItemDetailsScreen = ({navigation, route}) => {
     const item = route.params.item.item ? route.params.item.item : route.params.item;
     return (
         <View style={styles.container}>
-            <Text style={styles.heading}>Item: {item.heading}</Text>
-            <Text></Text>
-            <Text>Description:</Text>
-            <Text>{item.description}</Text>
-            <Text></Text>
-            <Image source={{ uri: item.image_url }} style={{ width: 200, height: 200 }} />
-            <Button title="Back" onPress={() => navigation.goBack()} />
+            <Background/>
+            <Image source={{uri: item.image_url}} style={styles.coverImage}/>
+            <View style={styles.itemsContainer}>
+                <Text style={styles.heading}>{item.heading}</Text>
+                <View style={{borderBottomColor: 'black', borderBottomWidth: 1, marginTop: 10, marginBottom: 10}}/>
+                <Text></Text>
+                <Text>Description:</Text>
+                <Text>{item.description}</Text>
+                <View style={{position: 'absolute', bottom: 0, width: "112%"}}>
+                    <Button label={"Back"} onPress={() => navigation.goBack()}
+                    borderRadius={20} backgroundColor={Colors.red20}/>
+                </View>
+            </View>
         </View>
     );
 }
@@ -22,13 +29,23 @@ const ItemDetailsScreen = ({ navigation, route }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
         paddingTop: 40,
     },
     itemsContainer: {
         flex: 1,
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 20,
+        margin: 20,
+        marginTop: 0,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
     },
     scrollBarItemsContainer: {
         flexDirection: "row",
@@ -38,7 +55,18 @@ const styles = StyleSheet.create({
     },
     heading: {
         fontWeight: 'bold',
+        fontSize: 20,
+        alignSelf: 'center',
     },
+    coverImage: {
+        width: '100%',
+        height: 200,
+        resizeMode: 'cover',
+        marginTop: 10,
+        marginBottom: 10,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+    }
 });
 
 export default ItemDetailsScreen;
