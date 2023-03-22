11efa1c962f1dbdfe44946bb2d1f38e593435599
SamuelAl <alarcocs@tcd.ie>
2023-03-22 17:05:50 +0000

    Delete button is not available for items that are not owned by the user.

--- frontend/screens/ItemDetailsScreen.tsx
+++ frontend/screens/ItemDetailsScreen.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {View, Text, Alert, Image} from 'react-native';
-import {Button, Colors} from 'react-native-ui-lib';
-import {StyleSheet} from 'react-native';
+import { View, Text, Alert, Image } from 'react-native';
+import { Button, Colors } from 'react-native-ui-lib';
+import { StyleSheet } from 'react-native';
 import Background from "../components/general/Background";
 
 import { app, getUserById } from '../Firebase';
@@ -16,64 +16,63 @@ const handleDeleteItem = async (item, callback) => {
         await deleteDoc(docRef)
         callback()
     }
-    catch(err) {
+    catch (err) {
         alert("Error deleting this item.")
     }
 }
 
-const ItemDetailsScreen = ({navigation, route}) => {
+const ItemDetailsScreen = ({ navigation, route }) => {
     const item = route.params.item.item ? route.params.item.item : route.params.item;
     return (
         <View style={styles.container}>
-            <Background/>
-            <Image source={{uri: item.image_url}} style={styles.coverImage}/>
+            <Background />
+            <Image source={{ uri: item.image_url }} style={styles.coverImage} />
             <View style={styles.itemsContainer}>
                 <Text style={styles.heading}>{item.heading}</Text>
-                <View style={{borderBottomColor: 'black', borderBottomWidth: 1, marginTop: 10, marginBottom: 10}}/>
+                <View style={{ borderBottomColor: 'black', borderBottomWidth: 1, marginTop: 10, marginBottom: 10 }} />
                 <Text></Text>
                 <Text>Description:</Text>
                 <Text>{item.description}</Text>
-            <BBButton label="Message owner" onPress={async () => {
+                <BBButton label="Message owner" onPress={async () => {
 
-                if (item.owner == route.params.userid) {
-                    Alert.alert("You can't create a conversation with yourself");
-                    return;
-                }
+                    if (item.owner == route.params.userid) {
+                        Alert.alert("You can't create a conversation with yourself");
+                        return;
+                    }
 
-                //arbitrary convention to have user1 be alphabetically before user2 in the chat document
-                const [user1, user2] = [item.owner, route.params.userid].sort();
+                    //arbitrary convention to have user1 be alphabetically before user2 in the chat document
+                    const [user1, user2] = [item.owner, route.params.userid].sort();
 
-                //look for a chat between the user and the owner
-                const allChats = await getDocs(query(collection(database, "chats"), where("user1", "==", user1)));
-                let existingChat = allChats.docs.find((document) => { return document.data().user2 == user2; });
+                    //look for a chat between the user and the owner
+                    const allChats = await getDocs(query(collection(database, "chats"), where("user1", "==", user1)));
+                    let existingChat = allChats.docs.find((document) => { return document.data().user2 == user2; });
 
-                //create a chat if it does not exist
-                let id;
-                if (!existingChat) {
-                    const newChat = await addDoc(collection(database, "chats"), {
-                        user1: user1,
-                        user2: user2
-                    });
-                    //create messages collection in the chat by adding a blank document
-                    await setDoc(doc(database, `chats/${newChat.id}/messages`, "0"), {
-                        sender: null,
-                        content: null
-                    });
-                    id = newChat.id;
-                }
-                else {
-                    id = existingChat.id;
-                }
+                    //create a chat if it does not exist
+                    let id;
+                    if (!existingChat) {
+                        const newChat = await addDoc(collection(database, "chats"), {
+                            user1: user1,
+                            user2: user2
+                        });
+                        //create messages collection in the chat by adding a blank document
+                        await setDoc(doc(database, `chats/${newChat.id}/messages`, "0"), {
+                            sender: null,
+                            content: null
+                        });
+                        id = newChat.id;
+                    }
+                    else {
+                        id = existingChat.id;
+                    }
 
-                //navigate to the new chat created (or existing)
-                const { displayName, image_url, email } = await getUserById(item.owner);
-                navigation.navigate("Messaging", { screen: "Messaging", params: { chat: { id: id, correspondant: { displayName: displayName, photoURL: image_url, email: email } }, userid: route.params.userid } })
-            }}></BBButton>
-            <BBButton label="Delete" onPress={async () => await handleDeleteItem(item, navigation.goBack)}/>
-
-                <View style={{position: 'absolute', bottom: 0, width: "112%"}}>
+                    //navigate to the new chat created (or existing)
+                    const { displayName, image_url, email } = await getUserById(item.owner);
+                    navigation.navigate("Messaging", { screen: "Messaging", params: { chat: { id: id, correspondant: { displayName: displayName, photoURL: image_url, email: email } }, userid: route.params.userid } })
+                }}></BBButton>
+                {item.owner == route.params.userid ? (<BBButton label="Delete" onPress={async () => await handleDeleteItem(item, navigation.goBack)} />) : (<></>)}
+                <View style={{ position: 'absolute', bottom: 0, width: "112%" }}>
                     <Button label={"Back"} onPress={() => navigation.goBack()}
-                    borderRadius={20} backgroundColor={Colors.red20}/>
+                        borderRadius={20} backgroundColor={Colors.red20} />
                 </View>
             </View>
         </View>
