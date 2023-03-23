ca20a6f71f44d98d19576c61a1b123f36129ca4b
Chafficui <felixbeinssen@gmail.com>
2023-03-23 00:15:55 +0000

    applied styling to messages

--- frontend/screens/ChatsScreen.tsx
+++ frontend/screens/ChatsScreen.tsx
@@ -1,18 +1,18 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { View, Button, FlatList } from 'react-native';
-import { StyleSheet } from 'react-native';
+import React, {useContext, useState, useEffect} from 'react';
+import {View, Button, FlatList} from 'react-native';
+import {StyleSheet} from 'react-native';
 import Heading1 from "../components/Heading1";
-import { AuthContext } from '../navigation/AuthProvider';
-import { app, getUserById } from '../Firebase';
-import { getFirestore, getDoc, getDocs, doc, query, collection, where } from "firebase/firestore";
+import {AuthContext} from '../navigation/AuthProvider';
+import {app, getUserById} from '../Firebase';
+import {getFirestore, getDoc, getDocs, doc, query, collection, where} from "firebase/firestore";
 
 import UserCard from "../components/messaging/UserCard";
 
 const database = getFirestore(app);
 
-const ChatsScreen = ({ navigation }) => {
+const ChatsScreen = ({navigation}) => {
 
-    const { user } = useContext(AuthContext);
+    const {user} = useContext(AuthContext);
 
     const [userData, setUserData] = useState({
         displayName: "User", photoURL: null, email: null
@@ -23,26 +23,33 @@ const ChatsScreen = ({ navigation }) => {
     useEffect(() => {
         (async () => {
             //get data for the current user
-            const { displayName, image_url = null, email } = await getUserById(user.uid);
-            setUserData({ displayName: displayName, photoURL: image_url, email: email });
+            const {displayName, image_url = null, email} = await getUserById(user.uid);
+            setUserData({displayName: displayName, photoURL: image_url, email: email});
 
             async function updateChats() {
                 const chats = [];
                 let chatsQuery = query(collection(database, "chats"), where("user1", "==", user.uid));
                 let chatDocs = await getDocs(chatsQuery);
                 for (const document of chatDocs.docs) {
-                    const { displayName, image_url = null, email } = await getUserById(document.data().user2);
-                    chats.push({ id: document.id, correspondant: { displayName: displayName, photoURL: image_url, email: email } });
+                    const {displayName, image_url = null, email} = await getUserById(document.data().user2);
+                    chats.push({
+                        id: document.id,
+                        correspondant: {displayName: displayName, photoURL: image_url, email: email}
+                    });
                 }
                 chatsQuery = query(collection(database, "chats"), where("user2", "==", user.uid));
                 chatDocs = await getDocs(chatsQuery);
                 for (const document of chatDocs.docs) {
-                    const { displayName, image_url = null, email } = await getUserById(document.data().user1);
-                    chats.push({ id: document.id, correspondant: { displayName: displayName, photoURL: image_url, email: email } });
+                    const {displayName, image_url = null, email} = await getUserById(document.data().user1);
+                    chats.push({
+                        id: document.id,
+                        correspondant: {displayName: displayName, photoURL: image_url, email: email}
+                    });
                 }
 
                 setChats(chats);
             }
+
             //load the chats with the screen
             await updateChats();
 
@@ -53,12 +60,18 @@ const ChatsScreen = ({ navigation }) => {
 
     return (
         <View style={styles.container}>
-            <Heading1 text={`${userData.displayName}'s chats`}></Heading1>
+            <View style={{paddingLeft: 20}}>
+                <Heading1 text={`${userData.displayName}'s chats`}></Heading1>
+            </View>
             <FlatList
                 data={chats}
-                renderItem={({ item }) => { return <UserCard user={item.correspondant} onPress={() => { navigation.navigate("Messaging", { chat: item, userid: user.uid }) }} /> }}
-                ItemSeparatorComponent={({ }) => {
-                    return <View style={{ height: 5 }}></View>;
+                renderItem={({item}) => {
+                    return <UserCard user={item.correspondant} onPress={() => {
+                        navigation.navigate("Messaging", {chat: item, userid: user.uid})
+                    }}/>
+                }}
+                ItemSeparatorComponent={({}) => {
+                    return <View style={{height: 5}}></View>;
                 }}
             />
         </View>
@@ -67,10 +80,10 @@ const ChatsScreen = ({ navigation }) => {
 
 const styles = StyleSheet.create({
     container: {
-        top: 35,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: '#fff',
+        paddingTop: 40,
     },
 });
 
--- frontend/screens/MessagingScreen.tsx
+++ frontend/screens/MessagingScreen.tsx
@@ -1,20 +1,21 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { View, KeyboardAvoidingView, Button, FlatList, Text, TextInput } from 'react-native';
-import { StyleSheet } from 'react-native';
-import { app } from '../Firebase';
-import { getFirestore, getDoc, getDocs, setDoc, doc, query, collection, where, onSnapshot } from "firebase/firestore";
+import React, {useContext, useState, useEffect} from 'react';
+import {View, KeyboardAvoidingView, FlatList, Text, TextInput} from 'react-native';
+import {StyleSheet} from 'react-native';
+import {app} from '../Firebase';
+import {Button, Colors} from 'react-native-ui-lib';
+import {getFirestore, getDoc, getDocs, setDoc, doc, query, collection, where, onSnapshot} from "firebase/firestore";
 
 import UserCard from '../components/messaging/UserCard';
+import BBButton from "../components/general/BBButton";
+import Background from "../components/general/Background";
 
 const database = getFirestore(app);
 
-const MessagingScreen = ({ navigation, route }) => {
+const MessagingScreen = ({navigation, route}) => {
 
     const [messages, setMessages] = useState([]);
 
     const [input, setInput] = useState("");
-
-    const Spacer = <View style={{ height: 5 }}></View>;
     useEffect(() => {
         (async () => {
             const messagesQuery = query(collection(database, `chats/${route.params.chat.id}/messages`));
@@ -22,10 +23,15 @@ const MessagingScreen = ({ navigation, route }) => {
             async function updateMessages() {
                 const messagesDocs = await getDocs(messagesQuery);
                 const messages = messagesDocs.docs
-                    .filter((document) => { return document.data().content && true })   //non-blank messages
-                    .map((document, index) => { console.log(document.data()); return { id: index, message: document.data() } });
+                    .filter((document) => {
+                        return document.data().content && true
+                    })   //non-blank messages
+                    .map((document, index) => {
+                        return {id: index, message: document.data()}
+                    });
                 setMessages(messages);
             }
+
             //load messages with screen
             updateMessages();
             //check for updates regularly
@@ -34,32 +40,26 @@ const MessagingScreen = ({ navigation, route }) => {
     }, []);
     return (
         <KeyboardAvoidingView behavior="position" style={styles.container}>
-            <View style={{ alignItems: "center" }}>
-                <UserCard user={route.params.chat.correspondant} onPress={() => { }}></UserCard>
+            <Background/>
+            <View style={styles.header}>
+                <UserCard backButton user={route.params.chat.correspondant} onPress={() => {
+                }}></UserCard>
             </View>
-            {Spacer}
-            <View style={{ borderColor: "#000", borderWidth: 5 }}>
-                <Button title="Go back" onPress={() => { navigation.navigate("Chats") }}></Button>
-            </View>
-            {Spacer}
             <FlatList
                 data={messages}
-                renderItem={({ item }) => {
+                renderItem={({item}) => {
                     if (item.message.sender == route.params.userid) {
-                        return <Text style={styles.user} >{`You: ${item.message.content}`}</Text>
-                    }
-                    else {
-                        return <Text style={styles.correspondant} >{`${route.params.chat.correspondant.displayName}: ${item.message.content}`}</Text>
+                        return <Text style={styles.user}>{`${item.message.content}`}</Text>
+                    } else {
+                        return <Text style={styles.correspondant}>{`${item.message.content}`}</Text>
                     }
                 }}
             />
-            {Spacer}
-            <View style={{ borderColor: "#000", borderWidth: 1 }}>
-                <TextInput placeholder="Enter a message" value={input} onChangeText={(text) => { setInput(text); }}></TextInput>
-            </View>
-            {Spacer}
-            <View style={{ borderColor: "#000", borderWidth: 5 }}>
-                <Button title="Send message" onPress={() => {
+            <View style={styles.inputContainer}>
+                <TextInput style={styles.input} placeholder="Enter a message" value={input} onChangeText={(text) => {
+                    setInput(text);
+                }}/>
+                <Button backgroundColor={Colors.green10} style={styles.sendButton} label="Send" onPress={() => {
                     //unix millis to have really low chance of two documents writing with the same id (lazy solution)
                     setDoc(doc(database, `chats/${route.params.chat.id}/messages`, `${Date.now()}`), {
                         content: input,
@@ -79,17 +79,66 @@ const bottomMargin = 15;
 
 const styles = StyleSheet.create({
     container: {
-        top: topMargin,
-        bottom: bottomMargin,
-        backgroundColor: "#fff",
+        flex: 1,
+        flexDirection: 'column',
+        paddingTop: 40,
+
+    },
+    header: {
+        position: "absolute",
+        top: 0,
+        left: 0,
     },
     user: {
         fontSize: fontsize,
-        backgroundColor: "#DFF",
+        marginTop: topMargin,
+        marginBottom: bottomMargin,
+        marginLeft: 10,
+        marginRight: 10,
+        textAlign: "right",
+        alignSelf: "flex-end",
+        backgroundColor: "#000",
+        color: "#fff",
+        borderRadius: 10,
+        padding: 10,
     },
     correspondant: {
         fontSize: fontsize,
-        backgroundColor: "#BEF",
+        marginTop: topMargin,
+        marginBottom: bottomMargin,
+        marginLeft: 10,
+        marginRight: 10,
+        textAlign: "left",
+        alignSelf: "flex-start",
+        backgroundColor: "#aaa",
+        color: "#000",
+        borderRadius: 10,
+        padding: 10,
+    },
+    input: {
+        fontSize: fontsize,
+        marginTop: topMargin,
+        marginBottom: bottomMargin,
+        marginLeft: 10,
+        marginRight: 10,
+        color: "#000",
+        borderRadius: 10,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: "#000",
+        height: 50,
+        width: "70%",
+    },
+    sendButton: {
+        height: 50,
+        width: "20%",
+        position: "relative",
+        top: 8,
+        right: 0,
+    },
+    inputContainer: {
+        flexDirection: "row",
+        alignItems: "center",
     },
 });
 
