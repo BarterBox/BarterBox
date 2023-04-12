e37ae9eafd363e24f824dd831805b96c3630fcb5
Chafficui <felixbeinssen@gmail.com>
2023-04-12 23:59:38 +0100

    fixed item overlapping

--- frontend/screens/ItemDetailsScreen.tsx
+++ frontend/screens/ItemDetailsScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { View, Text, Alert, Image } from 'react-native';
+import {View, Text, Alert, Image, ScrollView} from 'react-native';
 import { Button, Colors } from 'react-native-ui-lib';
 import { StyleSheet } from 'react-native';
 import Background from "../components/general/Background";
@@ -112,121 +112,123 @@ const ItemDetailsScreen = ({ navigation, route }) => {
             <Background />
             <Image source={{ uri: item.image_url }} style={styles.coverImage} />
             <View style={styles.itemsContainer}>
-                <Text style={styles.heading}>{item.heading}</Text>
-                <View style={{ borderBottomColor: 'black', borderBottomWidth: 1, marginTop: 10, marginBottom: 10 }} />
-                <Text></Text>
-                <Text>Owner</Text>
-                <Text>{ownerName}</Text>
-                <Text></Text>
-                <Text>Category:</Text>
-                <Text>{item.category}</Text>
-                <Text></Text>
-                <Text>Description:</Text>
-                <Text>{item.description}</Text>
-                <View style={{ borderBottomColor: 'black', borderBottomWidth: 1, marginTop: 10, marginBottom: 10 }} />
+                <ScrollView style={styles.scrollView}>
+                    <Text style={styles.heading}>{item.heading}</Text>
+                    <View style={{ borderBottomColor: 'black', borderBottomWidth: 1, marginTop: 10, marginBottom: 10 }} />
+                    <Text></Text>
+                    <Text>Owner</Text>
+                    <Text>{ownerName}</Text>
+                    <Text></Text>
+                    <Text>Category:</Text>
+                    <Text>{item.category}</Text>
+                    <Text></Text>
+                    <Text>Description:</Text>
+                    <Text>{item.description}</Text>
+                    <View style={{ borderBottomColor: 'black', borderBottomWidth: 1, marginTop: 10, marginBottom: 10 }} />
 
-                {item.owner != route.params.userid && <BBButton label="Message owner" onPress={async () => {
+                    {item.owner != route.params.userid && <BBButton label="Message owner" onPress={async () => {
 
-                    if (item.owner == route.params.userid) {
-                        Alert.alert("You can't create a conversation with yourself");
-                        return;
-                    }
-
-                    //arbitrary convention to have user1 be alphabetically before user2 in the chat document
-                    const [user1, user2] = [item.owner, route.params.userid].sort();
-
-                    //look for a chat between the user and the owner
-                    const allChats = await getDocs(query(collection(database, "chats"), where("user1", "==", user1)));
-                    let existingChat = allChats.docs.find((document) => { return document.data().user2 == user2; });
-
-                    //create a chat if it does not exist
-                    let id;
-                    if (!existingChat) {
-                        const newChat = await addDoc(collection(database, "chats"), {
-                            user1: user1,
-                            user2: user2
-                        });
-                        //create messages collection in the chat by adding a blank document
-                        await setDoc(doc(database, `chats/${newChat.id}/messages`, "0"), {
-                            sender: null,
-                            content: null
-                        });
-                        id = newChat.id;
-                    }
-                    else {
-                        id = existingChat.id;
-                    }
-
-                    //navigate to the new chat created (or existing)
-                    const { displayName, image_url, email } = await getUserById(item.owner);
-                    setChatRedirect({ chat: { id: id, correspondant: { displayName: displayName, photoURL: image_url, email: email, id: item.owner } }, userid: route.params.userid });
-                    navigation.navigate("Messages")
-                }}></BBButton>}
-
-                {item.owner == route.params.userid ?
-                    (<BBButton label="Delete" 
-                        onPress={async () => 
-                            await handleDeleteItem(item, navigation.goBack)
-                        } />)
-                    : (null)}
-                {(item.owner != route.params.userid && !item.borrowed) ?
-                    (<BBButton label="Request"
-                        onPress={async () => {
-                            await handleRequestItem(item,
-                                route.params.userid,
-                                () => alert("Item requested succesfully"))
+                        if (item.owner == route.params.userid) {
+                            Alert.alert("You can't create a conversation with yourself");
+                            return;
                         }
-                        } />
-                    ) : (null) }
-                {(item.owner == route.params.userid && item.borrowed && item.return_ready) ? (
-                    <BBButton label="Item Returned"
-                        onPress={async () => {
-                            await handleReturnItem(item, navigation.goBack())
-                        }}
-                    />
-                ) : (null)}
-                {(item.borrowed_by.id === route.params.userid && item.borrowed) ? (
-                    <BBButton label="Return Item"
-                        onPress={async () => {
-                            await handleItemReturn(item, navigation.goBack())
-                        }}
-                    />
-                ) : (null)}
-                {item.owner == route.params.userid && !item.borrowed && recentRequest != null ?
-                    (<>
-                        <View>
-                            <Text>
-                                Request by {recentRequest.requestedBy.displayName}
-                            </Text>
-                            <View style={styles.requestButtonsContainer}>
-                                <BBButton label={`Accept`}
-                                    onPress={() => handleAcceptRequest(item, recentRequest, () => alert("Request Accepted"))} />
-                                <BBButton label={`Reject`}
-                                    onPress={() => handleRejectRequest(item, recentRequest, () => {
-                                        alert("Request rejected")
-                                        setRefreshRequests(-refreshRequest)
-                                    })} />
+
+                        //arbitrary convention to have user1 be alphabetically before user2 in the chat document
+                        const [user1, user2] = [item.owner, route.params.userid].sort();
+
+                        //look for a chat between the user and the owner
+                        const allChats = await getDocs(query(collection(database, "chats"), where("user1", "==", user1)));
+                        let existingChat = allChats.docs.find((document) => { return document.data().user2 == user2; });
+
+                        //create a chat if it does not exist
+                        let id;
+                        if (!existingChat) {
+                            const newChat = await addDoc(collection(database, "chats"), {
+                                user1: user1,
+                                user2: user2
+                            });
+                            //create messages collection in the chat by adding a blank document
+                            await setDoc(doc(database, `chats/${newChat.id}/messages`, "0"), {
+                                sender: null,
+                                content: null
+                            });
+                            id = newChat.id;
+                        }
+                        else {
+                            id = existingChat.id;
+                        }
+
+                        //navigate to the new chat created (or existing)
+                        const { displayName, image_url, email } = await getUserById(item.owner);
+                        setChatRedirect({ chat: { id: id, correspondant: { displayName: displayName, photoURL: image_url, email: email, id: item.owner } }, userid: route.params.userid });
+                        navigation.navigate("Messages")
+                    }}></BBButton>}
+
+                    {item.owner == route.params.userid ?
+                        (<BBButton label="Delete"
+                                   onPress={async () =>
+                                       await handleDeleteItem(item, navigation.goBack)
+                                   } />)
+                        : (null)}
+                    {(item.owner != route.params.userid && !item.borrowed) ?
+                        (<BBButton label="Request"
+                                   onPress={async () => {
+                                       await handleRequestItem(item,
+                                           route.params.userid,
+                                           () => alert("Item requested succesfully"))
+                                   }
+                                   } />
+                        ) : (null) }
+                    {(item.owner == route.params.userid && item.borrowed && item.return_ready) ? (
+                        <BBButton label="Item Returned"
+                                  onPress={async () => {
+                                      await handleReturnItem(item, navigation.goBack())
+                                  }}
+                        />
+                    ) : (null)}
+                    {(item.borrowed_by.id === route.params.userid && item.borrowed) ? (
+                        <BBButton label="Return Item"
+                                  onPress={async () => {
+                                      await handleItemReturn(item, navigation.goBack())
+                                  }}
+                        />
+                    ) : (null)}
+                    {item.owner == route.params.userid && !item.borrowed && recentRequest != null ?
+                        (<>
+                            <View>
+                                <Text>
+                                    Request by {recentRequest.requestedBy.displayName}
+                                </Text>
+                                <View style={styles.requestButtonsContainer}>
+                                    <BBButton label={`Accept`}
+                                              onPress={() => handleAcceptRequest(item, recentRequest, () => alert("Request Accepted"))} />
+                                    <BBButton label={`Reject`}
+                                              onPress={() => handleRejectRequest(item, recentRequest, () => {
+                                                  alert("Request rejected")
+                                                  setRefreshRequests(-refreshRequest)
+                                              })} />
+                                </View>
                             </View>
-                        </View>
 
-                    </>) :
-                    (<></>)}
+                        </>) :
+                        (<></>)}
 
-                {item.owner == route.params.userid && item.borrowed  ?
-                    (<>
-                        <View>
-                            <Text>
-                                Currently borrowed by: {item.borrowed_by.displayName}
-                            </Text>
-                        </View>
+                    {item.owner == route.params.userid && item.borrowed  ?
+                        (<>
+                            <View>
+                                <Text>
+                                    Currently borrowed by: {item.borrowed_by.displayName}
+                                </Text>
+                            </View>
 
-                    </>) :
-                    (<></>)}
+                        </>) :
+                        (<></>)}
+                </ScrollView>
+            </View>
 
-                <View style={{ position: 'absolute', bottom: 0, width: "112%" }}>
-                    <Button label={"Back"} onPress={() => navigation.goBack()}
+            <View style={{ position: 'absolute', bottom: 20, left: "50%", transform: [{ translateX: -150 }], width: 300 }}>
+                <Button label={"Back"} onPress={() => navigation.goBack()}
                         borderRadius={20} backgroundColor={Colors.red20} />
-                </View>
             </View>
         </View>
     );
@@ -238,7 +240,6 @@ const styles = StyleSheet.create({
         paddingTop: 40,
     },
     itemsContainer: {
-        flex: 1,
         backgroundColor: 'white',
         padding: 20,
         borderRadius: 20,
@@ -252,6 +253,9 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.25,
         shadowRadius: 3.84,
         elevation: 5,
+    },
+    scrollView: {
+        height: '60%',
     },
     scrollBarItemsContainer: {
         flexDirection: "row",
